b76deeed71e445375f2e44ad5c54b4e2906a97e5
Harry Nicholls <harry.m.nicholls@gmail.com>
2019-03-01 16:56:15 -0500

    Adds basic GoalList component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Goal } from './components/goal/Goal';
+import { GoalList } from './components/goal-list/GoalList';
 
 class App extends Component {
   render() {
     return (
       <div className="pa2">
-        <Goal text="Climb Everest" dueDate="09/19/2019" />
+        <GoalList>
+          <Goal text="Climb Everest" dueDate="09/19/2019" />
+          <Goal text="Swim with sharks" dueDate="01/01/2020" />
+          <Goal text="Learn to juggle" dueDate="07/21/2019" />
+        </GoalList>
       </div>
     );
   }
---
+++ src/components/goal-list/GoalList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const GoalList = ({ children }) => {
+  return <ul className="list pl0 mv0">{children.map(child => (
+    <li className="mv1">
+      {child}
+    </li>
+  ))}</ul>;
+};
